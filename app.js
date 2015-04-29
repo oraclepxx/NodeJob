5cdb4d0239e2474a0d251be7334ee56898947378
oraclepxx <oraclepxx@gmail.com>
2015-04-29 14:58:42 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+/**
+ * Created by xinpan on 04/29/2015.
+ */
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multi(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}

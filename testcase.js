07a52a974b70f76d0047433066200c4d341b41cb
oraclepxx <oraclepxx@gmail.com>
2015-04-30 23:31:15 -0700

    -update project

--- testcase.js
+++ testcase.js
@@ -2,31 +2,27 @@
  * Created by xinpan on 04/30/2015.
  */
 
-//describe("A suite", function () {
-//    it("contains spec with an expectation", function () {
-//        expect(true).toBe(true);
-//    });
-//});
-//
-//describe("A suite is just a function", function () {
-//    var a;
-//
-//    it("and so is a spec", function () {
-//        a = true;
-//
-//        expect(a).toBe(true);
-//    });
-//});
-
-
 describe("A suite", function () {
 
-    it("and has a positive case", function () {
+    it("contains spec with an expectation", function () {
         expect(true).toBe(true);
+    });
+
+    var a;
+
+    it("and so is a spec", function () {
+        a = true;
+
+        expect(a).toBe(true);
+    });
+
+    it("and has a positive case", function () {
+        expect(true).toBe(false);
     });
 
     it("and can have a negative case", function () {
         expect(false).not.toBe(true);
     });
+
 });
 

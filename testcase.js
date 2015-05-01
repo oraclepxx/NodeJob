72f4d390e6c335a3fa6af7cb579a2a402e51dbdb
oraclepxx <oraclepxx@gmail.com>
2015-04-30 23:37:17 -0700

    -update project

--- testcase.js
+++ testcase.js
@@ -15,7 +15,7 @@ describe("A suite", function () {
     });
 
     it("and has a positive case", function () {
-        expect(true).toBe(false);
+        expect(true).toBe(true);
     });
 
     it("and can have a negative case", function () {

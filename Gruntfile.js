32030b57de543a00cd508a28c10e1649cd259975
oraclepxx <oraclepxx@gmail.com>
2015-05-01 10:46:49 -0700

    -update project

--- Gruntfile.js
+++ Gruntfile.js
@@ -12,17 +12,72 @@ module.exports = function (grunt) {
                 src: ['testcase.js'],
                 options: {
                     junit: {
-                        path: 'junit'
+                        path: 'junit',
+                        consolidate: false
                     }
                 }
             }
         }
     });
 
+
+    //grunt.initConfig({
+    //    jasmine_nodejs: {
+    //        // task specific (default) options
+    //        pkg: grunt.file.readJSON('package.json'),
+    //        options: {
+    //            specNameSuffix: "spec.js", // also accepts an array
+    //            helperNameSuffix: "helper.js",
+    //            useHelpers: true,
+    //            // configure one or more built-in reporters
+    //            reporters: {
+    //                console: {
+    //                    colors: true,
+    //                    cleanStack: 1,       // (0|false)|(1|true)|2|3
+    //                    verbosity: 3,        // (0|false)|1|2|(3|true)
+    //                    listStyle: "indent", // "flat"|"indent"
+    //                    activity: false
+    //                },
+    //                junit: {
+    //                     savePath: "",
+    //                     filePrefix: "junit-report-",
+    //                     consolidate: true,
+    //                     useDotNotation: true
+    //                }
+    //                // nunit: {
+    //                //     savePath: "./reports",
+    //                //     filename: "nunit-report.xml",
+    //                //     reportName: "Test Results"
+    //                // },
+    //                // terminal: {
+    //                //     color: false,
+    //                //     showStack: false,
+    //                //     verbosity: 2
+    //                // },
+    //                // teamcity: true,
+    //                // tap: true
+    //            }
+    //            // add custom Jasmine reporter(s)
+    //            //customReporters: []
+    //        }
+    //        //your_target: {
+    //        //    // target specific options
+    //        //    options: {
+    //        //        useHelpers: false
+    //        //    },
+    //        //    // specs files
+    //        //    specs: [
+    //        //        'specs/*.js',
+    //        //    ]
+    //        //}
+    //    }
+    //});
+
     // Load the plugin that provides the "uglify" task.
-    grunt.loadNpmTasks('grunt-contrib-jasmine');
+    //grunt.loadNpmTasks('grunt-contrib-jasmine');
+    grunt.loadNpmTasks('grunt-jasmine-nodejs');
 
     // Default task(s).
-    grunt.registerTask('default', ['jasmine']);
+    grunt.registerTask('nodejob', ['jasmine_nodejs']);
 
 };

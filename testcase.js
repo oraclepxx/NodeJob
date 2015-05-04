62ac5f675f35f3b5df36cedf7efc5b8c2ae68c01
oraclepxx <oraclepxx@gmail.com>
2015-05-04 16:42:45 -0700

    -update project

--- testcase.js
+++ testcase.js
@@ -1,93 +1,93 @@
-/**
- * Created by xinpan on 04/30/2015.
- */
-
-var should = require('should');
-
-function square(num) {
-    return num * num;
-}
-
-
-describe('A suite', function () {
-
-    //it("contains specs with an expectation", function () {
-    //    expect(true).should.toBe(true);
-    //});
-    //
-    //it("and so is a specs", function () {
-    //    var a = true;
-    //
-    //    expect(a).toBe(true);
-    //});
-    //
-    //it("and has a positive case", function () {
-    //    expect(true).toBe(true);
-    //});
-    //
-    //it("and can have a negative case", function () {
-    //    expect(false).not.toBe(true);
-    //});
-
-    it('test function square()', function () {
-        expect(square(2)).toBe(4);
-    });
-
-});
-
-
-//var add = function(a, b) {
-//    return a + b;
+///**
+// * Created by xinpan on 04/30/2015.
+// */
+//
+//var should = require('should');
+//
+//function square(num) {
+//    return num * num;
 //}
 //
-//function subtract(a, b) {
-//    return a - b;
-//}
 //
-//function divide(a, b) {
-//    return a / b;
-//}
+//describe('A suite', function () {
 //
-//function multiply(a, b) {
-//    return a * b;
-//}
-//
-//describe('A Suite', function () {
-//    var a = 10;
-//    var b = 2;
-//
-//    it('should add numbers', function () {
-//        var result = add(a, b);
-//        expect(result).toEqual(12);
-//        // this is a custom matcher implemented within our helper.
-//        //expect(result).toBePositive();
-//    });
-//
-//    //it('should execute async specs...', function (done) {
-//    //    var result = add(a, b);
-//    //    setTimeout(function () {
-//    //        expect(result).toEqual(12);
-//    //        done();
-//    //    }, 4000);
+//    //it("contains specs with an expectation", function () {
+//    //    expect(true).should.toBe(true);
+//    //});
+//    //
+//    //it("and so is a specs", function () {
+//    //    var a = true;
+//    //
+//    //    expect(a).toBe(true);
+//    //});
+//    //
+//    //it("and has a positive case", function () {
+//    //    expect(true).toBe(true);
+//    //});
+//    //
+//    //it("and can have a negative case", function () {
+//    //    expect(false).not.toBe(true);
 //    //});
 //
-//    it('should subtract numbers', function () {
-//        var result = subtract(b, a);
-//        expect(result).toEqual(-8);
-//        // this is another custom matcher implemented within our helper.
-//        //expect(result).toBeNegative();
-//    });
-//
-//    it('should multiply numbers (final)', function () {
-//        var result = multiply(a, b);
-//        expect(result).toEqual(20);
-//        //pending('this is the pending reason');
-//    });
-//
-//    it('should divide numbers', function () {
-//        var result = divide(a, b);
-//        expect(result).toEqual(5); // should be 5 so this will throw
+//    it('test function square()', function () {
+//        expect(square(2)).toBe(4);
 //    });
 //
 //});
-
+//
+//
+////var add = function(a, b) {
+////    return a + b;
+////}
+////
+////function subtract(a, b) {
+////    return a - b;
+////}
+////
+////function divide(a, b) {
+////    return a / b;
+////}
+////
+////function multiply(a, b) {
+////    return a * b;
+////}
+////
+////describe('A Suite', function () {
+////    var a = 10;
+////    var b = 2;
+////
+////    it('should add numbers', function () {
+////        var result = add(a, b);
+////        expect(result).toEqual(12);
+////        // this is a custom matcher implemented within our helper.
+////        //expect(result).toBePositive();
+////    });
+////
+////    //it('should execute async specs...', function (done) {
+////    //    var result = add(a, b);
+////    //    setTimeout(function () {
+////    //        expect(result).toEqual(12);
+////    //        done();
+////    //    }, 4000);
+////    //});
+////
+////    it('should subtract numbers', function () {
+////        var result = subtract(b, a);
+////        expect(result).toEqual(-8);
+////        // this is another custom matcher implemented within our helper.
+////        //expect(result).toBeNegative();
+////    });
+////
+////    it('should multiply numbers (final)', function () {
+////        var result = multiply(a, b);
+////        expect(result).toEqual(20);
+////        //pending('this is the pending reason');
+////    });
+////
+////    it('should divide numbers', function () {
+////        var result = divide(a, b);
+////        expect(result).toEqual(5); // should be 5 so this will throw
+////    });
+////
+////});
+//

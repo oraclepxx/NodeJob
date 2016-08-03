34109f72c18b8e40894c5256ec5dd88b82f27e9f
oraclepxx <oraclepxx@gmail.com>
2016-08-03 14:53:10 -0700

    Update app.js

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ function divide(a, b) {
     return a / b;
 }
 
-console.log("1 + 2 = " + add(1, 2));
+console.log("1 + 3 = " + add(1, 2));
 console.log("1 - 2 = " + subtract(1, 2));
 console.log("1 * 2 = " + multi(1, 2));
 console.log("1 / 2 = " + divide(1, 2));
